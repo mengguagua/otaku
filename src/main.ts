07a42b728dc20e2525069605f9e278f5d1706b4d
gaocc <15957108449@163.com>
2023-12-07 10:12:24 +0800

    fix 修改端口

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // 增加全局的校验管道，校验接口入参字段
-  await app.listen(3000);
+  await app.listen(3100);
 }
 bootstrap();
